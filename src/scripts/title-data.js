97ccfcbb05095e20faef7d95b1e64aac501953d9
Harri Klingsten <harri.klingsten2@scb.se>
2025-08-10 19:18:06 +0200

    added titles

---
+++ src/scripts/title-data.js
@@ -0,0 +1,47 @@
+[
+  {
+    "name": "WBA",
+    "fullName": "World Boxing Association",
+    "region": "Global"
+  },
+  {
+    "name": "WBC",
+    "fullName": "World Boxing Council",
+    "region": "Global"
+  },
+  {
+    "name": "IBF",
+    "fullName": "International Boxing Federation",
+    "region": "Global"
+  },
+  {
+    "name": "WBO",
+    "fullName": "World Boxing Organization",
+    "region": "Global"
+  },
+  {
+    "name": "EBU",
+    "fullName": "European Boxing Union",
+    "region": "Europe"
+  },
+  {
+    "name": "ABU",
+    "fullName": "African Boxing Union",
+    "region": "Africa"
+  },
+  {
+    "name": "NABF",
+    "fullName": "North American Boxing Federation",
+    "region": "North America"
+  },
+  {
+    "name": "SABC",
+    "fullName": "South American Boxing Championship",
+    "region": "South America"
+  },
+  {
+    "name": "OPBF",
+    "fullName": "Oriental and Pacific Boxing Federation",
+    "region": "Asia"
+  }
+]
