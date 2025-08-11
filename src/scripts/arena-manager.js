8cb5761cf01fd70542b1932e0f2d8be1562e1862
Harri Klingsten <harri.klingsten2@scb.se>
2025-08-11 12:43:34 +0200

    Adjusted rule for selecting arena

--- src/scripts/arena-manager.js
+++ src/scripts/arena-manager.js
@@ -3,14 +3,14 @@ import { ARENAS } from './arena-data.js';
 export class ArenaManager {
   static getRandomArena(highestRank) {
     let prestige;
-    if (highestRank > 7) {
-      prestige = 1;
-    } else if (highestRank > 2) {
-      prestige = 2;
+    if (highestRank <= 2) {
+      prestige = 3;        
+    } else if (highestRank <= 7) {
+      prestige = 2;        
     } else {
-      prestige = 1;
+      prestige = 1;        
     }
-    const options = ARENAS.filter((a) => a.Prestige === prestige);
+    const options = ARENAS.filter(a => a.Prestige === prestige);
     const index = Phaser.Math.Between(0, options.length - 1);
     return options[index];
   }
