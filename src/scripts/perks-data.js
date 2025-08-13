275020551d8658741c0fe9652e1de414b22b6994
Harri Klingsten <harri.klingsten2@scb.se>
2025-08-13 19:00:07 +0200

    added perk data

--- src/scripts/perks-data.js
+++ src/scripts/perks-data.js
@@ -2,5 +2,7 @@ export const PERKS = [
   { "Name": "Playbook", "Level": 1, "Price": 20000 },
   { "Name": "Playbook", "Level": 2, "Price": 50000 },
   { "Name": "Playbook", "Level": 3, "Price": 50000 },
-  { "Name": "Change", "Level": 1, "Price": 100000 }
+  { "Name": "Change", "Level": 1, "Price": 100000 },
+  { "Name": "Standard", "Level": 1, "Price": 50000 },
+  { "Name": "Offensive", "Level": 1, "Price": 100000 }
 ];
