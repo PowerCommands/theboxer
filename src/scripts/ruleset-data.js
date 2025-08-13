b86893bc5194f2f76f992b1314e9ec4f1ca61724
Harri Klingsten <harri.klingsten2@scb.se>
2025-08-13 19:01:08 +0200

    rulest data changes

--- src/scripts/ruleset-data.js
+++ src/scripts/ruleset-data.js
@@ -1,5 +1,5 @@
 export const RULESETS = {
   1: { id: 1, name: 'Beginner', bonus: null, perk: null },
-  2: { id: 2, name: 'Standard', bonus: { ability: 'stamina', value: 0.2 }, perk: 'Fightplan standard' },
-  3: { id: 3, name: 'Offensive', bonus: { ability: 'power', value: 0.2 }, perk: 'Fightplan Offense' },
+  2: { id: 2, name: 'Standard', bonus: { ability: 'stamina', value: 0.2 }, perk: 'Standard' },
+  3: { id: 3, name: 'Offensive', bonus: { ability: 'power', value: 0.2 }, perk: 'Offensive' },
 };
